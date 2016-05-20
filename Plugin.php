745fb21c16f4fa21713edb63aa043151272b85b8
Trias Nur Rahman <contact@triasrahman.com>
2016-05-20 18:55:13 +0700

    Make it editable with Builder plugin

--- Plugin.php
+++ Plugin.php
@@ -8,21 +8,6 @@ use System\Classes\PluginBase;
 class Plugin extends PluginBase
 {
 
-    /**
-     * Returns information about this plugin.
-     *
-     * @return array
-     */
-    public function pluginDetails()
-    {
-        return [
-            'name'        => 'Octommerce',
-            'description' => 'No description provided yet...',
-            'author'      => 'Trias Nur Rahman',
-            'icon'        => 'icon-shopping-cart'
-        ];
-    }
-
     public function register()
     {
         //
@@ -46,58 +31,6 @@ class Plugin extends PluginBase
     {
         return [
             //
-        ];
-    }
-
-    public function registerNavigation()
-    {
-        return [
-            'products' => [
-                'label'       => 'Products',
-                'url'         => \Backend::url('octommerce/octommerce/products'),
-                'icon'        => 'icon-tags',
-                'permissions' => ['octommerce.octommerce.*'],
-                'order'       => 30,
-
-                'sideMenu' => [
-                    'products' => [
-                        'label'       => 'All Products',
-                        'icon'        => 'icon-barcode',
-                        'url'         => \Backend::url('octommerce/octommerce/products'),
-                        'permissions' => ['octommerce.octommerce.access_products']
-                    ],
-
-                    'attributes' => [
-                        'label'       => 'Attributes',
-                        'icon'        => 'icon-flag',
-                        'url'         => \Backend::url('octommerce/octommerce/attributes'),
-                        'permissions' => ['octommerce.octommerce.access_products']
-                    ],
-
-                    'categories' => [
-                        'label'       => 'Categories',
-                        'icon'        => 'icon-table',
-                        'url'         => \Backend::url('octommerce/octommerce/categories'),
-                        'permissions' => ['octommerce.octommerce.access_products']
-                    ],
-
-                    'lists' => [
-                        'label'       => 'Lists',
-                        'icon'        => 'icon-reorder',
-                        'url'         => \Backend::url('octommerce/octommerce/lists'),
-                        'permissions' => ['octommerce.octommerce.access_products']
-                    ],
-
-                ]
-            ],
-
-            'order' => [
-                'label'       => 'Orders',
-                'url'         => \Backend::url('octommerce/octommerce/orders'),
-                'icon'        => 'icon-shopping-cart',
-                'permissions' => ['octommerce.octommerce.*'],
-                'order'       => 40,
-            ],
         ];
     }
 
---
+++ lang/en/lang.php
@@ -0,0 +1,6 @@
+<?php return [
+    'plugin' => [
+        'name' => 'Octommerce',
+        'description' => 'This is the most awesome e-commerce plugin.',
+    ],
+];
