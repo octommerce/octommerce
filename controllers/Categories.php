3479bc5a6cb3c8a01bbe6717ec0a5d00b08d8faa
Trias Nur Rahman <contact@triasrahman.com>
2016-06-14 14:33:29 +0700

    Add relation categories & product lists backend

--- controllers/Categories.php
+++ controllers/Categories.php
@@ -11,11 +11,13 @@ class Categories extends Controller
 {
     public $implement = [
         'Backend.Behaviors.FormController',
-        'Backend.Behaviors.ListController'
+        'Backend.Behaviors.ListController',
+        'Backend.Behaviors.RelationController',
     ];
 
     public $formConfig = 'config_form.yaml';
     public $listConfig = 'config_list.yaml';
+    public $relationConfig = 'config_relation.yaml';
 
     public function __construct()
     {
