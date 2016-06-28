3dcf328c0b33f6ecb5f7e56c733ac1e4f5dcc5e1
Trias Nur Rahman <contact@triasrahman.com>
2016-06-29 01:22:28 +0700

    create daily reports graph

--- controllers/Reports.php
+++ controllers/Reports.php
@@ -1,7 +1,10 @@
 <?php namespace Octommerce\Octommerce\Controllers;
 
+use Db;
 use BackendMenu;
+use Carbon\Carbon;
 use Backend\Classes\Controller;
+use Octommerce\Octommerce\Models\Order;
 
 /**
  * Reports Back-end Controller
@@ -26,5 +29,40 @@ class Reports extends Controller
     {
         $this->asExtension('ListController')->index();
         $this->bodyClass = 'compact-container';
+
+        $this->vars['dataPaidOrders'] = $this->getLastOrdersData();
     }
+
+    protected function getLastOrdersData($status = 'paid', $duration = 30)
+    {
+
+        $date = Carbon::now()->subDays($duration);
+
+        // lists() does not accept raw queries,
+        // so you have to specify the SELECT clause
+        $days = Order::select(array(
+                Db::raw('DATE(`created_at`) as `date`'),
+                Db::raw('SUM(subtotal) as `amount`')
+            ))
+            ->whereStatusCode($status)
+            ->where('created_at', '>', $date)
+            ->groupBy('date')
+            ->orderBy('date', 'ASC')
+            ->lists('amount', 'date');
+
+        $points = [];
+
+        for ($i = $duration; $i > 0; $i--) {
+           $date = Carbon::now()->subDays($i);
+
+           $points[] = [
+               $date->timestamp * 1000,
+               isset($days[$date->format('Y-m-d')]) ? $days[$date->format('Y-m-d')] : 0,
+           ];
+        }
+
+        // Parse format
+        return str_replace('"', '', substr(substr(json_encode($points), 1), 0, -1));
+    }
+
 }
