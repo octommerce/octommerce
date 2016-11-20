88fca236c6065bae47c9e09a874c77ae03e9305e
Trias Nur Rahman <contact@triasrahman.com>
2016-11-21 01:14:52 +0700

    Update default components

--- components/Cart.php
+++ components/Cart.php
@@ -8,11 +8,13 @@ use Cart as CartHelper;
 use Cms\Classes\ComponentBase;
 use Octommerce\Octommerce\Models\Cart as CartModel;
 use Octommerce\Octommerce\Models\Product;
+use Octommerce\Octommerce\Models\Settings;
 
 class Cart extends ComponentBase
 {
 
     public $cart;
+    public $settings;
 
     public function componentDetails()
     {
@@ -29,13 +31,15 @@ class Cart extends ComponentBase
 
     public function onRun()
     {
-
         $this->cart = CartModel::whereSessionId(Session::getId())->first();
+        $this->settings = Settings::instance();
     }
 
     public function onAdd()
     {
         $cart = $this->page['cart'] = CartHelper::addItem(post('product_id'), post('qty') ?: 1);
+
+        $this->page['settings'] = Settings::instance();
 
         return [
             'result' => 'Product successfully added to cart.',
@@ -48,6 +52,8 @@ class Cart extends ComponentBase
     {
         $cart = $this->page['cart'] = CartHelper::updateItem(post('product_id'), post('qty'));
 
+        $this->page['settings'] = Settings::instance();
+
         return [
             'result' => 'Cart is successfully updated.',
             '.cart-counter' => $cart->count_qty,
@@ -58,6 +64,8 @@ class Cart extends ComponentBase
     public function onRemove()
     {
         $cart = $this->page['cart'] = CartHelper::removeItem(post('product_id'));
+
+        $this->page['settings'] = Settings::instance();
 
         return [
             'result' => 'Product successfully removed from cart.',
@@ -70,6 +78,8 @@ class Cart extends ComponentBase
     {
         $cart = $this->page['cart'] = CartHelper::clear();
 
+        $this->page['settings'] = Settings::instance();
+
         return [
             'result' => 'Cart is successfully cleared.',
             '.cart-counter' => $cart->count_qty,
@@ -80,6 +90,8 @@ class Cart extends ComponentBase
     public function onRefresh()
     {
         $cart = $this->page['cart'] = CartHelper::get();
+
+        $this->page['settings'] = Settings::instance();
 
         // TODO:
         // Calculate stock availibity of every products.
