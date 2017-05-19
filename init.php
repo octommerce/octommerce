04896c617384f140677c1241ed80ab263e16c028
Surahman <surahman.duang@gmail.com>
2017-05-19 13:31:31 +0700

    Take out evoucher product type
    In case, let user extend product type instead of add default evoucher

--- init.php
+++ init.php
@@ -5,8 +5,6 @@
  **/
 Event::listen('cart.beforeAddItem', 'Octommerce\Octommerce\Listeners\TriggerProductType@beforeAddToCart');
 Event::listen('cart.afterAddItem', 'Octommerce\Octommerce\Listeners\TriggerProductType@afterAddToCart');
-Event::listen('octommerce.octommerce.productType.invoicePaidProcessed', 'Octommerce\Octommerce\Listeners\SendVoucherToCustomer');
-Event::listen('octommerce.octommerce.productType.invoicePaidProcessed', 'Octommerce\Octommerce\Listeners\PaidToCompletedOnEvoucher');
 
 /**
  * Resonsiv.pay
