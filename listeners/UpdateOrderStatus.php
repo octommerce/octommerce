7dbd2afb5e5000678d3c4a1dcf0862a03396a157
Surahman <surahman.duang@gmail.com>
2017-08-16 14:47:04 +0700

    Avoid to update status if order has been paid

--- listeners/UpdateOrderStatus.php
+++ listeners/UpdateOrderStatus.php
@@ -13,8 +13,13 @@ class UpdateOrderStatus
          * Only process if 
          *  - order_statuses table has the same status code
          *  - new status not same with existing order status
+         *  - order status has never been paid
          **/
-        if ( ! $orderStatus or $invoice->related->status_code == $orderStatus->code) return;
+        if ( 
+            ! $orderStatus or
+            $invoice->related->status_code == $orderStatus->code or
+            $invoice->related->isPaid()
+        ) return;
 
         $invoice->related->updateStatus($orderStatus->code, $record->comment);
     }
